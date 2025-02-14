2e13553be30b6ee424824e904bc7119a1ad0565c
Durdaking <Jamesimcglade2@gmail.com>
2025-02-14 14:17:03 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,82 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Player1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 8 8 8 8 . . . . . . . 
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . . 8 f 8 8 f 8 . . . . . . 
+. . . . 8 8 8 8 8 8 . . . . . . 
+. . . . 8 f f f f 8 . . . . . . 
+. . . . . 8 8 8 8 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "Player1 Mode 3":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 7 . . . . . . . . . 
+. . . . 7 7 7 7 7 7 . . . . . . 
+. . . 7 7 7 7 7 7 7 7 7 7 . . . 
+. . . 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 f 7 7 7 7 f 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 f 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 f 7 7 7 7 7 f f 7 7 . . 
+. . 7 7 f f f 7 7 f f 7 7 7 . . 
+. . . 7 7 7 f f f f 7 7 7 . . . 
+. . . . 7 7 7 7 7 7 7 . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "Player1 Mode2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . 2 2 2 2 2 2 . . 
+. . . 2 . . . 2 2 2 2 2 2 2 2 . 
+. . . 2 2 2 2 2 2 2 2 2 2 2 2 . 
+. . 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+. . . 2 2 2 2 f f 2 2 2 2 2 2 2 
+. . . 2 2 2 2 f f 2 2 2 f f 2 2 
+. . . 2 2 2 2 2 2 2 2 2 f f 2 2 
+. . . 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. . . 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. . . 2 2 2 2 2 2 2 2 2 2 2 2 . 
+. . . 2 2 2 2 2 2 2 2 2 2 2 2 . 
+. . . 2 2 2 2 2 2 2 2 2 2 2 . . 
+. . . . . . . 2 2 2 2 2 2 . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,63 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Player1.isHittingTile(CollisionDirection.Bottom)) {
+        Player1.vy = -275
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Player1.isHittingTile(CollisionDirection.Bottom)) {
+        Player1.vy = -200
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenSwitchUp, function (sprite, location) {
+    if (controller.A.isPressed()) {
+        tiles.setCurrentTilemap(tilemap`Secret Room`)
+        tiles.placeOnTile(Player1, tiles.getTileLocation(1, 14))
+    }
+    if (controller.B.isPressed()) {
+        tiles.setCurrentTilemap(tilemap`Crate Room`)
+        tiles.placeOnTile(Player1, tiles.getTileLocation(1, 14))
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
+    game.gameOver(false)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.purpleSwitchDown, function (sprite, location) {
+    game.showLongText("Press A to win", DialogLayout.Bottom)
+    if (controller.A.isPressed()) {
+        game.gameOver(true)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenSwitchDown, function (sprite, location) {
+    game.showLongText("Congrats on you go to the secret room press A", DialogLayout.Bottom)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+    game.showLongText("Press A to open", DialogLayout.Bottom)
+    if (controller.A.isPressed()) {
+        game.showLongText("GREEDY NOW YOU DIE", DialogLayout.Full)
+        game.gameOver(false)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
+    game.gameOver(false)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.purpleSwitchUp, function (sprite, location) {
+    game.setDialogTextColor(7)
+    if (controller.A.isPressed()) {
+        Player1.setImage(assets.image`Player1 Mode2`)
+    }
+    if (controller.B.isPressed()) {
+        Player1.setImage(assets.image`Player1 Mode 3`)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairLarge, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`Ending Room`)
+    tiles.placeOnTile(Player1, tiles.getTileLocation(1, 14))
+})
+let Player1: Sprite = null
+Player1 = sprites.create(assets.image`Player1`, SpriteKind.Player)
+controller.moveSprite(Player1, 100, 0)
+Player1.ay = 500
+scene.cameraFollowSprite(Player1)
+tiles.placeOnTile(Player1, tiles.getTileLocation(1, 18))
+tiles.setCurrentTilemap(tilemap`Start Level1`)
+game.showLongText("Never open a chest press A or B to do things when touching the levers or the chest Press A to jump And B to SUPER JUMP", DialogLayout.Bottom)
